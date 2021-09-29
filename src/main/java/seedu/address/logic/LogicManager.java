7ef0fe035665a6ad81012998a1bd45ac0beba7e4
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-09-30 00:10:03 +0800

    Scaffold the `PersonAdapter` for Client View.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -74,5 +75,10 @@ public class LogicManager implements Logic {
     @Override
     public void setGuiSettings(GuiSettings guiSettings) {
         model.setGuiSettings(guiSettings);
+    }
+
+    @Override
+    public PersonAdapter getPersonAdapter(Index i) {
+        return new PersonAdapter(model, model.getFilteredPersonList().get(i.getZeroBased()));
     }
 }
