deaa7756a5143b7ba37193d1c997a8c6b0237151
sheehui <tansheehui@gmail.com>
2021-10-10 02:04:15 +0800

    Map escape key to redirect to home page
    Do a universal mapping of escape key to default DonnaFin
    home page.
    
    Note the universal part!
    
    In the future, mapping of escape keys if we go for edit popup might
    interfere with this function.

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
@@ -52,6 +53,13 @@ public class MainWindow extends UiPart<Stage> {
     private StackPane statusbarPlaceholder;
 
     /**
+     * A wrapper around contents of DonnaFin view. Mainly for the global escape key function
+     * to direct users to home page.
+     */
+    @FXML
+    private StackPane viewWrapper;
+
+    /**
      * Creates a {@code MainWindow} with the given {@code Stage} and {@code Logic}.
      */
     public MainWindow(Stage primaryStage, UiManager uiManager) {
@@ -65,6 +73,8 @@ public class MainWindow extends UiPart<Stage> {
         setWindowDefaultSize(uiManager.getGuiSettings());
 
         setAccelerators();
+
+        setHandleEsc();
 
         helpWindow = new HelpWindow();
     }
@@ -128,6 +138,18 @@ public class MainWindow extends UiPart<Stage> {
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
 
+
+    /**
+     * Repopulate viewFinderPlaceholder with person list.
+     * Redirects users from any part of DonnaFin to home page.
+     */
+    public void setHandleEsc() {
+        viewWrapper.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                fillInnerParts();
+            }
+        });
+    }
 
     /**
      * Switches the children inside the holder with the tab
