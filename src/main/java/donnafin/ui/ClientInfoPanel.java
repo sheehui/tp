89a0386278ee8aeea48c6382e878315ec3851d32
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-10-12 00:03:02 +0800

    Change ListView to VBox for ClientInfo

--- src/main/java/donnafin/ui/ClientInfoPanel.java
+++ src/main/java/donnafin/ui/ClientInfoPanel.java
@@ -6,16 +6,15 @@ import donnafin.logic.PersonAdapter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
 public class ClientInfoPanel extends UiPart<Region> {
     private static final String FXML = "ClientInfoPanel.fxml";
     private PersonAdapter personAdapter;
 
     @FXML
-    private ListView<AttributePanel> clientInfoList;
+    private VBox clientInfoList;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
@@ -24,31 +23,8 @@ public class ClientInfoPanel extends UiPart<Region> {
         super(FXML);
         this.personAdapter = personAdapter;
         // Make all the attributes into FXML AttributePanel
-        ObservableList<AttributePanel> attributePanelObservableList =
-                personAdapter.getAllAttributesList().stream()
-                        .map(x -> new AttributePanel(x))
-                        .collect(Collectors.toCollection(FXCollections::observableArrayList));
-        clientInfoList.setItems(attributePanelObservableList);
-        clientInfoList.setCellFactory(listView -> new AttributeListViewCell());
-    }
-
-    class AttributeListViewCell extends ListCell<AttributePanel> {
-        @Override
-        protected void updateItem(AttributePanel attribute, boolean empty) {
-            super.updateItem(attribute, empty);
-
-            if (empty || attribute == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(attribute.getRoot());
-            }
-        }
-    }
-
-    /**
-     * Handles the Enter button pressed event.
-     */
-    private void handleCommandEntered(String attributeChanged) {
+        personAdapter.getAllAttributesList().stream()
+                .map(x -> new AttributePanel(x).getRoot())
+                .forEach(y -> clientInfoList.getChildren().add(y));
     }
 }
