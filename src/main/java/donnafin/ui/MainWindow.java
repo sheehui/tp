3d963b85af636bc69f4758092132321873d08e51
sheehui <tansheehui@gmail.com>
2021-10-12 11:16:31 +0800

    Remove status bar footer in GUI

--- src/main/java/donnafin/ui/MainWindow.java
+++ src/main/java/donnafin/ui/MainWindow.java
@@ -133,9 +133,6 @@ public class MainWindow extends UiPart<Stage> {
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(uiManager.getAddressBookFilePath());
-        statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
-
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
