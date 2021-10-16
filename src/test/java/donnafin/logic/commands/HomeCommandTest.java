d2665b97db518dfe628f290f82f40e1171a7ac25
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-10-16 20:33:52 +0800

    Add Home Command testing.

--- src/test/java/donnafin/logic/commands/HomeCommandTest.java
+++ src/test/java/donnafin/logic/commands/HomeCommandTest.java
@@ -1,23 +1,24 @@
 package donnafin.logic.commands;
 
 import static donnafin.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static donnafin.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
-
-import org.junit.jupiter.api.Test;
+import static donnafin.logic.commands.HomeCommand.MESSAGE_SUCCESS;
 
 import donnafin.model.AddressBook;
 import donnafin.model.Model;
 import donnafin.model.ModelManager;
 import donnafin.model.UserPrefs;
 import donnafin.ui.Ui;
+import org.junit.jupiter.api.Test;
 
-public class HelpCommandTest {
+
+public class HomeCommandTest {
     private final Model model = new ModelManager(new AddressBook(), new UserPrefs(), null);
     private final Model expectedModel = new ModelManager(new AddressBook(), new UserPrefs(), null);
 
     @Test
-    public void execute_help_success() {
-        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, Ui::showHelp);
-        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    public void execute_home_success() {
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_SUCCESS, Ui::showHome);
+        assertCommandSuccess(new HomeCommand(), model, expectedCommandResult, expectedModel);
     }
 }
+
