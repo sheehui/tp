4463545ddb85811fd334451a2d6a050e2fea91ec
bharathcs <bharathcs@users.noreply.github.com>
2021-10-16 14:21:52 +0800

    Update src/test/java/guitests/guihandles/ClientInfoPanelHandle.java

--- src/test/java/guitests/guihandles/ClientInfoPanelHandle.java
+++ src/test/java/guitests/guihandles/ClientInfoPanelHandle.java
@@ -4,7 +4,7 @@ import javafx.scene.Node;
 
 public class ClientInfoPanelHandle extends NodeHandle<Node> {
 
-    public static final String CLIENT_INFO_PANEL_ID = "#clientInfoList";
+    public static final String CLIENT_INFO_PANEL_ID = "#container";
     // the above means that it will actually pick out a child component, the vbox containing attribute list
     // instead of the real ClientInfoPanel. Required for GUI Test to have a target.
 
