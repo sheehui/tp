db3cfda56202769e4ad79e129f105595fc261b42
DonnyS57 <donnysuriyanto@gmail.com>
2021-10-21 21:23:42 +0800

    Update UG to add home and tab as well as updating dg to include new use cases

--- src/main/java/donnafin/ui/AttributeTable.java
+++ src/main/java/donnafin/ui/AttributeTable.java
@@ -114,8 +114,6 @@ public class AttributeTable<T> extends UiPart<Region> {
         data = FXCollections.observableArrayList(collection);
         table.getColumns().addAll(columns);
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        System.out.println(columns.size());
-        System.out.println(table.getColumns().size());
         table.setItems(data);
     }
 
