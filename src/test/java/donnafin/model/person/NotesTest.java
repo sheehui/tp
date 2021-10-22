b3adb622b12b125cc8bcc9034ddf6cc3d4b98295
sheehui <tansheehui@gmail.com>
2021-10-22 15:00:16 +0800

    Update Notes test

--- src/test/java/donnafin/model/person/NotesTest.java
+++ src/test/java/donnafin/model/person/NotesTest.java
@@ -3,10 +3,20 @@ package donnafin.model.person;
 import org.junit.jupiter.api.Test;
 
 import static donnafin.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NotesTest {
+
+    @Test
+    public void constructor_validInputs_matchesExpectedString() {
+        assertEquals("loves kopi", new Notes("loves kopi").toString());
+        assertEquals("loves kopi\n\n\nloves chai", new Notes("loves kopi\n\n\nloves chai").toString());
+        assertEquals("loves kopi", new Notes("   loves kopi   ").toString());
+        assertEquals("loves kopi\n\n\nloves chai", new Notes("\nloves kopi\n\n\nloves chai\n\n").toString());
+        assertEquals("loves kopi\n\n\nloves chai", new Notes(" \n  loves kopi\n\n\nloves chai\n\n   ").toString());
+    }
 
     @Test
     public void constructor_null_throwsNullPointerException() {
@@ -22,6 +32,7 @@ public class NotesTest {
         assertFalse(Notes.isValidNotes("    client has bad temper")); // whitespace at the beginning
         assertFalse(Notes.isValidNotes("   very wealthy   ")); // whitespace at the end and beginning
         assertFalse(Notes.isValidNotes("loves ice cream     ")); // whitespace at the end
+        assertFalse(Notes.isValidNotes("    "));
 
         // valid notes
         assertTrue(Notes.isValidNotes("Joshua is a family-oriented client. Has a great preference for crypto"
