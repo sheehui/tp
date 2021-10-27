3a2841a13a8539c0da0b0dba0c8ce3b7670e468f
bharathcs <bharathcs@users.noreply.github.com>
2021-10-27 17:04:41 +0800

    Update EditCommand.java

--- src/main/java/donnafin/logic/commands/EditCommand.java
+++ src/main/java/donnafin/logic/commands/EditCommand.java
@@ -31,8 +31,7 @@ public class EditCommand extends Command {
             + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
-            + "[" + PREFIX_TAG + "TAG]...\n"
-            + "Example: " + COMMAND_WORD + " 1 "
+            + "Example: " + COMMAND_WORD
             + PREFIX_PHONE + "91234567 "
             + PREFIX_EMAIL + "johndoe@example.com";
 
