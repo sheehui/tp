7bbde72089c78fa5048ebf0253d91a3ac7f4f045
sheehui <tansheehui@gmail.com>
2021-11-03 01:58:37 +0800

    Handle valid commands in wrong window

--- src/main/java/donnafin/commons/core/Messages.java
+++ src/main/java/donnafin/commons/core/Messages.java
@@ -13,5 +13,11 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_USE_HELP_COMMAND = String.format(
             "Invalid command format! Try using the help command. \n%1$s", HelpCommand.MESSAGE_USAGE);
+    public static final String MESSAGE_COMMAND_NOT_IN_HOMEWINDOW = "This is a client window command and is not "
+            + "available in home window!\nTry using the view command to access client window commands."
+            + "\nRefer to our user guide for more info.";
+    public static final String MESSAGE_COMMAND_NOT_IN_CLIENTWINDOW = "This is a home window command and is not "
+            + "available in client window!\nTry using the home command to access home window commands."
+            + "\nRefer to our user guide for more info.";
 
 }
