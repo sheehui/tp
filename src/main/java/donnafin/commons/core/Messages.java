44749f41ff1db39d833c986c5744924ac3e75cb6
mrmrinal <mrinalganesh@gmail.com>
2021-11-02 17:45:56 +0800

    Fix Person not Persons bug when 1 person is listed

--- src/main/java/donnafin/commons/core/Messages.java
+++ src/main/java/donnafin/commons/core/Messages.java
@@ -11,6 +11,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_PERSON_LISTED_OVERVIEW = "1 person listed!";
+    public static final String MESSAGE_NO_PERSON_LISTED_OVERVIEW = "No person listed!";
     public static final String MESSAGE_USE_HELP_COMMAND = String.format(
             "Invalid command format! Try using the help command. \n%1$s", HelpCommand.MESSAGE_USAGE);
 
