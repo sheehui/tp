662fb3a150d166129f2f9d1d0d9253aa64b6139d
mrmrinal <mrinalganesh@gmail.com>
2021-11-03 17:31:54 +0800

    Add EditCommandParserTest

---
+++ src/test/java/donnafin/logic/parser/EditCommandParserTest.java
@@ -0,0 +1,70 @@
+package donnafin.logic.parser;
+
+import donnafin.logic.PersonAdapter;
+import donnafin.logic.commands.exceptions.CommandException;
+import donnafin.logic.parser.exceptions.ParseException;
+import donnafin.model.Model;
+import donnafin.model.ModelManager;
+import donnafin.model.UserPrefs;
+import donnafin.model.person.Address;
+import donnafin.model.person.Email;
+import donnafin.model.person.Name;
+import donnafin.model.person.Person;
+import donnafin.model.person.Phone;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static donnafin.testutil.TypicalPersons.getTypicalAddressBook;
+import static donnafin.testutil.TypicalPersons.getTypicalPersons;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EditCommandParserTest {
+
+    private PersonAdapter personAdapter;
+    private Model model;
+    private Person person;
+    private EditCommandParser parser;
+
+    @BeforeEach
+    private void reset() {
+        person = getTypicalPersons().get(0);
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), null);
+        this.personAdapter = new PersonAdapter(model, person);
+        parser = new EditCommandParser(personAdapter);
+    }
+    
+    @Test
+    public void parseCommand_changesName() throws CommandException, ParseException {
+        parser.parse("edit n/Allison Wang").execute(model);
+        assertEquals(new Name("Allison Wang"),
+                model.getFilteredPersonList().get(0).getName());
+     }
+     
+    @Test
+    public void parseCommand_changesPhone() throws CommandException, ParseException {
+        parser.parse("edit p/90725679").execute(model);
+        assertEquals(new Phone("90725679"),
+                model.getFilteredPersonList().get(0).getPhone());
+    }
+    
+    @Test
+    public void parseCommand_changesEmail() throws CommandException, ParseException {
+        parser.parse("edit e/allison@email.com").execute(model);
+        assertEquals(new Email("allison@email.com"),
+                model.getFilteredPersonList().get(0).getEmail());
+    }
+    
+    @Test
+    public void parseCommand_changesAddress() throws CommandException, ParseException {
+        parser.parse("edit a/759 Yishun Street 72").execute(model);
+        assertEquals(new Address("759 Yishun Street 72"),
+                model.getFilteredPersonList().get(0).getAddress());
+    }
+    
+    @Test
+    public void parseCommand_invalidFieldPrefix() throws CommandException, ParseException {
+        assertThrows(ParseException.class,() -> parser.parse("edit m/Allison Wang").execute(model));
+    }
+    
+}
