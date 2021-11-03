5ef94003cbe304c77ad6d48be07ccac1c9b2cba3
mrmrinal <mrinalganesh@gmail.com>
2021-11-03 20:04:50 +0800

    Fix bug where no output shown when Asset remarks are not specified

--- src/main/java/donnafin/logic/parser/AppendCommandParser.java
+++ src/main/java/donnafin/logic/parser/AppendCommandParser.java
@@ -91,7 +91,12 @@ public class AppendCommandParser implements Parser<AppendCommand> {
             throw new ParseException(Asset.MESSAGE_CONSTRAINTS);
         }
 
-        Asset newAsset = new Asset(name, type, value.toString(), remarks);
+        Asset newAsset = null;
+        try {
+            newAsset = new Asset(name, type, value.toString(), remarks);
+        } catch (IllegalArgumentException e) {
+            throw new ParseException(e.getMessage());
+        }
         return new AppendCommand(personAdapter, newAsset);
     }
 
