764738a8220f3221c8c1b4b076038c52e6ec2f53
mrmrinal <mrinalganesh@gmail.com>
2021-11-03 17:31:17 +0800

    Edit ViewCommandParser for invalid test case

--- src/test/java/donnafin/logic/parser/ViewCommandParserTest.java
+++ src/test/java/donnafin/logic/parser/ViewCommandParserTest.java
@@ -5,13 +5,29 @@ import donnafin.logic.parser.exceptions.ParseException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ViewCommandParserTest {
     private final ViewCommandParser parser = new ViewCommandParser();
     
     @Test
-    public void parser_invalidCommandFormat_throwsError() {
-        assertThrows(ParseException.class, () -> parser.parse("view"));
+    public void parseCommand_emptyNumberFormat_throwsError() {
+        assertThrows(ParseException.class, () -> parser.parse(""));
+    }
+    
+    @Test
+    public void parseCommand_returns_viewCommand() throws ParseException{
+        assertTrue(parser.parse("1") instanceof ViewCommand);
+    }
+    
+    @Test 
+    public void parseCommand_numberFormatException_throwsError() {
+        assertThrows(ParseException.class, () -> parser.parse("0"));
+    }
+    
+    @Test
+    public void parseCommand_invalidCommandFormat_throwsError() {
+        assertThrows(ParseException.class, () -> parser.parse("6a"));
     }
     
 }
