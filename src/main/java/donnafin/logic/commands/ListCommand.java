5418ba2a2699a3fa44e92c4659e6ed5c3c3730e0
mrmrinal <mrinalganesh@gmail.com>
2021-11-08 18:36:27 +0800

    Fix bug with list command clients

--- src/main/java/donnafin/logic/commands/ListCommand.java
+++ src/main/java/donnafin/logic/commands/ListCommand.java
@@ -12,12 +12,14 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "All clients listed!";
+    public static final String MESSAGE_NO_CLIENTS_FOUND = "No clients found.";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        int clientCount = model.getAddressBook().getPersonList().size();
+        return new CommandResult(clientCount == 0 ? MESSAGE_NO_CLIENTS_FOUND : MESSAGE_SUCCESS);
     }
 }
